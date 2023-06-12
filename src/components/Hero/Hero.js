4fe7bee65a9a0d05eabe31bffa47f35d5ff79750
Ebu98 <91473642+Ebu98@users.noreply.github.com>
2023-06-12 17:51:35 +0100

    changes made

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,6 @@ const Hero = (props) => (
         <SectionText>
        I'm excited to have the opportunity to connect with recruiters like you. As a full stack developer with a passion for front-end design, I'm always looking for new opportunities to showcase my skills and collaborate with innovative teams. Thank you for taking the time to review my work, and I look forward to hearing from you soon.
         </SectionText>
-        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
       </LeftSection>
     </Section>
   </>
